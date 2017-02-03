8208deba459ee336b3f449d55869343687ce49b8
git-directory-deploy <ryanbarron243@gmail.com>
2017-02-03 11:34:34 -0500

    Added new key testing if key is hidden from github commits and pushes

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -4,6 +4,16 @@ var Handlebars = require('handlebars');
 var githubtoken = require('./gitapikey.js')
 
 // send auth toeken to github if token is provided... which it is.
+
+$.ajax('https://api.github.com/users/<username>').done(function(){
+
+});
+
+
+
+
+
+
 if(githubtoken !== undefined){
   $.ajaxSetup({
     headers: {
