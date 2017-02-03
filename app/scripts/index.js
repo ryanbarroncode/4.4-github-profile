b262c0eeb596cc71e899ecc088510cb7c5dc9afe
Ryan Barron <ryanbarron243@gmail.com>
2017-02-03 10:16:09 -0500

    first commit

---
+++ app/scripts/index.js
@@ -0,0 +1,13 @@
+var $ = require('jquery');
+var _ = require('underscore');
+var Handlebars = require('handlebars');
+var githubtoken = require('./gitapikey.js')
+
+// send auth toeken to github if token is provided... which it is.
+if(githubtoken !== undefined){
+  $.ajaxSetup({
+    headers: {
+      'Authorization': 'token' + githubtoken.token
+    }
+  });
+}
