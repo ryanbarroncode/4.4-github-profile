4ab3640e7def148de7742a054e4917231443ac17
git-directory-deploy <ryanbarron243@gmail.com>
2017-02-03 19:34:04 -0500

    All content is on the page. Styling is needed and page scaling is needed.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,44 +1,49 @@
-var $ = require('jquery');
+var $ = window.$ = window.jQuery = require('jquery');
 // var _ = require('underscore');
 var Handlebars = require('handlebars');
-var githubtoken = require('./gitapikey.js')
+var githubtoken = require('./gitapikey.js');
+// var bootstrap = require('bootstrap-sass');
+var moment = require('moment')
 
 // send auth toeken to github if token is provided... which it is.
+if (githubtoken !== undefined) {
+    $.ajaxSetup({
+        headers: {
+            'Authorization': 'token ' + githubtoken.token
+        }
+    });
+}
+
 
 $.ajax('https://api.github.com/users/ryanbarroncode').done(function(data) {
     console.log(data);
 
-
-
     var source = $('#profile-template').html();
     var template = Handlebars.compile(source);
+
     $(".profile-info").append(template(data));
 });
 
 $.ajax('https://api.github.com/users/ryanbarroncode/repos').done(function(data) {
-  console.log(data);
+    console.log(data);
     var source = $('#repo-template').html();
     var template = Handlebars.compile(source);
 
     data.forEach(function(item) {
-      console.log(item);
-
-            $(".repo-info").append(template(item));
+        console.log(item);
+        var formatDate = moment(item.updated_at, "YYYYMMDD").fromNow();
+        item.formatDate = formatDate;
+        $(".repo-info").append(template(item));
     });
+});
+
+$.ajax('https://api.github.com/users/ryanbarroncode/orgs').done(function(data) {
+  console.log(data);
+  var source = $('#org-template').html();
+  var template = Handlebars.compile(source);
 });
 
 // var profileInfo = {
 //
 //   // profilePic: data.atavar_url
 // }
-
-
-
-
-if (githubtoken !== undefined) {
-    $.ajaxSetup({
-        headers: {
-            'Authorization': 'token' + githubtoken.token
-        }
-    });
-}
